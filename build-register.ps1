326b354eeb27dc10eddf33cfb1c3d74ae5110c89
Dave <crazeydave@users.noreply.github.com>
2026-04-06 03:30:11 +0100

    Added password a param.

--- build-register.ps1
+++ build-register.ps1
@@ -1,3 +1,10 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$KeyStorePassword
+)
+
 dotnet restore TheBleedingDeacons.Unity.Intergroup\TheBleedingDeacons.Unity.Intergroup.csproj -p:TargetFramework=net9.0-android
 dotnet restore TheBleedingDeacons.Intergroup.Register\TheBleedingDeacons.Intergroup.Register.csproj -p:TargetFramework=net9.0-android
-dotnet publish TheBleedingDeacons.Intergroup.Register\TheBleedingDeacons.Intergroup.Register.csproj -f net9.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=badi.keystore -p:AndroidSigningKeyAlias=intergroup-register -p:AndroidSigningKeyPass=100635 -p:AndroidSigningStorePass=100635
+dotnet publish TheBleedingDeacons.Intergroup.Register\TheBleedingDeacons.Intergroup.Register.csproj -f net9.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=badi.keystore -p:AndroidSigningKeyAlias=intergroup-register -p:AndroidSigningKeyPass=$KeyStorePassword -p:AndroidSigningStorePass=$KeyStorePassword
+
+Copy-Item TheBleedingDeacons.Intergroup.Register\bin\Release\net9.0-android\publish\*.apk C:\Data\dev\register\ -Force
