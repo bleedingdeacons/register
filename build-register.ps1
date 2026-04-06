d5ea4f2995ad565010690712a6c337584142f05a
Dave <crazeydave@users.noreply.github.com>
2026-04-06 04:08:16 +0100

    moved keystore.

--- build-register.ps1
+++ build-register.ps1
@@ -5,6 +5,6 @@ param(
 
 dotnet restore TheBleedingDeacons.Unity.Intergroup\TheBleedingDeacons.Unity.Intergroup.csproj -p:TargetFramework=net9.0-android
 dotnet restore TheBleedingDeacons.Intergroup.Register\TheBleedingDeacons.Intergroup.Register.csproj -p:TargetFramework=net9.0-android
-dotnet publish TheBleedingDeacons.Intergroup.Register\TheBleedingDeacons.Intergroup.Register.csproj -f net9.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=badi.keystore -p:AndroidSigningKeyAlias=intergroup-register -p:AndroidSigningKeyPass=$KeyStorePassword -p:AndroidSigningStorePass=$KeyStorePassword
+dotnet publish TheBleedingDeacons.Intergroup.Register\TheBleedingDeacons.Intergroup.Register.csproj -f net9.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=..\..\badi.keystore -p:AndroidSigningKeyAlias=badi -p:AndroidSigningKeyPass=$KeyStorePassword -p:AndroidSigningStorePass=$KeyStorePassword
 
 Copy-Item TheBleedingDeacons.Intergroup.Register\bin\Release\net9.0-android\publish\*.apk C:\Data\dev\register\ -Force
